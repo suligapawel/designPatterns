41a1241a2deaff2f51bbedcb6b5834baef5a42b1
psul00 <pawel.suliga@outlook.com>
2019-01-07 14:56:25 +0100

    add composite

---
+++ designPatterns/Structural/Composite/Abstracts/Component.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace designPatterns.Structural.Composite.Abstracts
+{
+    public abstract class Component
+    {
+        protected List<Component> _children;
+        public string Name { get; }
+
+        public Component(string name, bool isLeaf)
+        {
+            Name = name;
+
+            if (!isLeaf)
+                _children = new List<Component>();
+        }
+
+        public void AddChild(Component child)
+        {
+            if (_children != null)
+                _children.Add(child);
+        }
+
+        public abstract void Operation();
+    }
+}
---
+++ designPatterns/Structural/Composite/Components/TrainerComposite.cs
@@ -0,0 +1,19 @@
+﻿using designPatterns.Structural.Composite.Abstracts;
+using System;
+
+namespace designPatterns.Structural.Composite.Components
+{
+    public class TrainerComposite : Component
+    {
+        public TrainerComposite(string name, bool isLeaf) : base(name, isLeaf) { } 
+
+        public override void Operation()
+        {
+            foreach (var footballer in _children)
+            {
+                Console.WriteLine($"Trainer {Name} trains the {footballer.Name}");
+                footballer.Operation();
+            }
+        }
+    }
+}
