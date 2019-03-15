7b789931d1e1ab7415000fd66fff4fda8e9a5d45
psul00 <psul00@me.pl>
2019-03-15 12:39:18 +0100

    addTemplateMethod

---
+++ designPatterns/Behavioral/TemplateMethod/ConcreteClasses/EmotionalPlayer.cs
@@ -0,0 +1,18 @@
+﻿using designPatterns.Behavioral.TemplateMethod.Abstracts;
+using System;
+
+namespace designPatterns.Behavioral.TemplateMethod.ConcreteClasses
+{
+    public class EmotionalPlayer : BasePenaltyShooter
+    {
+        protected override void BeHappy()
+        {
+            Console.WriteLine("Player pulls the shirt and runs to the fans.");
+        }
+
+        protected override void BeSad()
+        {
+            Console.WriteLine("Player crying.");
+        }
+    }
+}
---
+++ designPatterns/Behavioral/TemplateMethod/TemplateMethodClient.cs
@@ -0,0 +1,58 @@
+﻿using designPatterns.Behavioral.TemplateMethod.Abstracts;
+using designPatterns.Behavioral.TemplateMethod.ConcreteClasses;
+using System;
+using System.Collections.Generic;
+
+namespace designPatterns.Behavioral.TemplateMethod
+{
+    public class TemplateMethodClient : IClient
+    {
+        private readonly List<BasePenaltyShooter> _penaltyShooters;
+        private int _firstTeamScored = 0;
+        private int _secondTeamScored = 0;
+
+        public TemplateMethodClient()
+        {
+            _penaltyShooters = new List<BasePenaltyShooter>
+            {
+                new ApatheticPlayer(),
+                new ApatheticPlayer(),
+                new EmotionalPlayer(),
+                new ApatheticPlayer(),
+                new EmotionalPlayer(),
+                new EmotionalPlayer(),
+                new ApatheticPlayer(),
+                new EmotionalPlayer(),
+                new EmotionalPlayer(),
+                new ApatheticPlayer()
+            };
+        }
+
+        public void RunClientCode()
+        {
+            PlayPenaltyContest();
+            Console.WriteLine($"WYNIK KOŃCOWY: {_firstTeamScored} : {_secondTeamScored}");
+
+        }
+
+        private void PlayPenaltyContest()
+        {
+            int goal = 0;
+
+            for (int i = 0; i < _penaltyShooters.Count; i++)
+            {
+                goal = _penaltyShooters[i].ShootPenalty() ? 1 : 0;
+
+                if (i % 2 == 0)
+                {
+                    _firstTeamScored += goal;
+                }
+                else
+                {
+                    _secondTeamScored += goal;
+                }
+                Console.WriteLine(string.Empty);
+            }
+        }
+    }
+}
